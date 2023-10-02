2bc54a3d19fcf848a98544e6a0c0ff9d46c81ba7
Francisco Menendez <fmenendezwork@gmail.com>
2023-10-02 16:44:50 -0700

    configure cors and retrieve user object based on session

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,12 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+    credentials: true,
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
