94f185b2dfadbc2b555b001b4ae1f39ff35569b6
Francisco Menendez <fmenendezwork@gmail.com>
2023-10-03 15:05:06 -0700

    retrive use object based on jwt

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -2,13 +2,17 @@ import { createContext, useState, useEffect } from "react";
 
 export const CurrentUser = createContext();
 
-__;
 function CurrentUserProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
   useEffect(() => {
     const getLoggedInUser = async () => {
       let response = await fetch(
-        "http://localhost:5000/authentication/profile"
+        "http://localhost:5000/authentication/profile",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
       );
       let user = await response.json();
       setCurrentUser(user);
